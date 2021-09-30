1940cd8d274811c081cfd33f9de1c9e8facdcd14
Tirtho 304 <shirdartho15-13304@diu.edu.bd>
2021-09-30 10:23:58 +0600

    change3

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 //import 'package:pracday4/pages/App.dart';
 //import 'package:pracday4/pages/bar.dart';
 // ignore: camel_case_types
@@ -20,47 +21,53 @@ class _loginState extends State<login> {
           
         ),
         drawer: Drawer(),
-        body:  Column(
-        children: <Widget> [
-          Image.asset("image/tirtho.jpg"),
-          SizedBox(height: 10,),
-          Text("Welcome",
-          style: TextStyle(
-            fontSize: 15,
-            fontWeight: FontWeight.bold,
-          ),
-          ),
-          SizedBox(height: 10,),
-         Padding(
-           padding: const EdgeInsets.symmetric(vertical: 16.0,horizontal: 32.0),
-           child: Column(
-             children: <Widget>[
-                TextFormField(
-            decoration: InputDecoration(
-              hintText: "Enter UseerName",
-              labelText: "UsserName"
-            ),
-          ),
-          TextFormField(
-            obscureText: true,
-            decoration: InputDecoration(
-              hintText: "Enter UseerName",
-              labelText: "UsserName",
-              
-              
-            ),
+        body:  SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
             
-          ),
-          SizedBox(height: 10,),
-          ElevatedButton(onPressed: (){}, child:Title(color: Colors.amber,
-           child: Text("Login")) )
-             ],
+          children: <Widget> [
+            Image.asset("image/tirtho.jpg"),
+            SizedBox(height: 10,),
+            Text("Welcome",
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+            ),
+            ),
+            SizedBox(height: 10,),
+           Padding(
              
-           ),
-         )
-        ],
-        
-      ),
+             padding: const EdgeInsets.symmetric(vertical: 16.0,horizontal: 32.0),
+             child: Column(
+               
+               children: <Widget>[
+                  TextFormField(
+              decoration: InputDecoration(
+                hintText: "Enter UseerName",
+                labelText: "UsserName"
+              ),
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "Enter UseerName",
+                labelText: "UsserName",
+                
+                
+              ),
+              
+            ),
+            SizedBox(height: 10,),
+            ElevatedButton(onPressed: (){}, child:Title(color: Colors.amber,
+             child: Text("Login")) )
+               ],
+               
+             ),
+           )
+          ],
+          
+              ),
+        ),
         ),
        
       );
